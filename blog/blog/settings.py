f10f40920cc7e0941a94f60df15855625940debb
SukoJim <tirgh2192@gmail.com>
2023-10-09 05:56:30 +0900

    Fix : set code time to Asia/Seoul

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog_home",
-    "login",
 ]
 
 MIDDLEWARE = [
@@ -107,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Seoul"
 
 USE_I18N = True
 
