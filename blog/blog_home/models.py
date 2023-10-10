0ca93540d5fe7be6937d56917fea9375bdb379f5
SukoJim <tirgh2192@gmail.com>
2023-10-10 23:19:44 +0900

    Add : Added post fields in model.py

--- blog/blog_home/models.py
+++ blog/blog_home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Post(models.Model):
+    # 기존 필드들
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    # 새로운 필드들
+    viewCount = models.PositiveIntegerField(default=0)
+
+    createdTimastamp = models.DateTimeField(default=timezone.now)
+    modifiedTimestamp = models.DateTimeField(auto_now=True)
