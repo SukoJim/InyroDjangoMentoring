f47524c812a8d5d09effb714223ded1db48dec1b
SukoJim <tirgh2192@gmail.com>
2023-11-14 18:11:30 +0900

    Transfer HTMLs: 'blog_home' to 'posts', delete 'blog_home'

--- blog/blog_home/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BlogHomeConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "blog_home"
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Create your views here.
+def blog_home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def postDetail(request):
+    return render(request, 'postDetail.html')
